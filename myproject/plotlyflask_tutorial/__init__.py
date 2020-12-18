af77edf5408a90712cf16aa8fc0ec8fb4da36424
sjsharksfan1991 <76226084+sjsharksfan1991@users.noreply.github.com>
2020-12-18 12:33:41 -0800

    Add files via upload

---
+++ myproject/plotlyflask_tutorial/__init__.py
@@ -0,0 +1,21 @@
+"""Initialize Flask app."""
+from flask import Flask
+from ddtrace import patch_all
+from flask import Flask
+from flask_assets import Environment
+patch_all()
+
+def init_app():
+    """Construct core Flask application with embedded Dash app."""
+    app = Flask(__name__, instance_relative_config=False)
+    app.config.from_object('config.Config')
+
+    with app.app_context():
+        # Import parts of our core Flask app
+        from . import routes
+
+        # Import Dash application
+        from .plotlydash.dashboard import init_dashboard
+        app = init_dashboard(app)
+
+        return app
